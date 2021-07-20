4694caf4d5efa8f51391611662a3e16079df1439
Andrew Luashchuk <tengro@gmail.com>
2021-07-20 16:39:54 +0300

    Added schedulers

--- api/larp_in_ua/apps/accounts/telegrambot.py
+++ api/larp_in_ua/apps/accounts/telegrambot.py
@@ -6,7 +6,7 @@ from larp_in_ua.apps.accounts.selectors import get_user_by_telegram_id
 from larp_in_ua.apps.common.utils.help import get_help_message
 from larp_in_ua.apps.common.utils.safe_send import safe_message_send
 from larp_in_ua.apps.scheduler.models import EventRegistration
-from larp_in_ua.apps.scheduler.utils import prepare_events_all, prepare_events_left, prepare_lections_all, prepare_lections_left
+# from larp_in_ua.apps.scheduler.utils import prepare_events_all, prepare_events_left, prepare_lections_all, prepare_lections_left
 
 
 import logging
@@ -50,36 +50,36 @@ def error(update, context):
     logger.warn('Update "%s" caused error "%s"' % (update, error))
 
 
-def registered_events_left(update, context):
-    telegram_id = update.message.chat_id
-    result = get_user_by_telegram_id(telegram_id)
-    if result:
-        result.send_personal_schedule()
-    else:
-        safe_message_send(context.bot, update.message.chat_id, text="Здається, ви ще не зареєструвалися?")
+# def registered_events_left(update, context):
+#     telegram_id = update.message.chat_id
+#     result = get_user_by_telegram_id(telegram_id)
+#     if result:
+#         result.send_personal_schedule()
+#     else:
+#         safe_message_send(context.bot, update.message.chat_id, text="Здається, ви ще не зареєструвалися?")
 
 
-def lections_left(update, context):
-    safe_message_send(context.bot, update.message.chat_id, text=prepare_lections_left())
+# def lections_left(update, context):
+#     safe_message_send(context.bot, update.message.chat_id, text=prepare_lections_left())
 
 
-def all_events_left(update, context):
-    rows = prepare_events_left()
-    if rows:
-        for item in row:
-            safe_message_send(context.bot, update.message.chat_id, text=item)
-    else:
-        safe_message_send(context.bot, update.message.chat_id, text="Здається, потік \"Діло\" добігає кінця!")
+# def all_events_left(update, context):
+#     rows = prepare_events_left()
+#     if rows:
+#         for item in row:
+#             safe_message_send(context.bot, update.message.chat_id, text=item)
+#     else:
+#         safe_message_send(context.bot, update.message.chat_id, text="Здається, потік \"Діло\" добігає кінця!")
 
 
-def all_schedule(update, context):
-    lections = prepare_lections_all()
-    rows = prepare_events_all()
-    if not rows:
-        rows = ["Здається, потік \"Діло\" добігає кінця!",]
-    data = [lections, ] + rows
-    for data_point in data:
-        safe_message_send(context.bot, update.message.chat_id, text=data_point)
+# def all_schedule(update, context):
+#     lections = prepare_lections_all()
+#     rows = prepare_events_all()
+#     if not rows:
+#         rows = ["Здається, потік \"Діло\" добігає кінця!", ]
+#     data = [lections, ] + rows
+#     for data_point in data:
+#         safe_message_send(context.bot, update.message.chat_id, text=data_point)
 
 
 def main():
